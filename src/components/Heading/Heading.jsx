04e2b07838706a7733257e28e4c950e9afed0e90
darrk-heart <prospaul999@gmail.com>
2024-03-18 15:24:26 +0000

    header structure done

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -6,13 +6,32 @@ function Heading() {
   return (
     <div className={styles.background}>
       <img src={wallpaper} alt="background side bar" />
-      <div className={styles.text}>
-        <div className={styles.texts}>
-          <span>1</span>
-          <div>
-            <span>STEP 1</span>
-            <span>YOUR INFO</span>
-          </div>
+      <div className={styles.texts}>
+        <span className={styles.texts_number}>1</span>
+        <div>
+          <span className={styles.step}>STEP 1</span>
+          <span className={styles.title}>YOUR INFO</span>
+        </div>
+      </div>
+      <div className={styles.texts}>
+        <span className={styles.texts_number}>2</span>
+        <div>
+          <span className={styles.step}>STEP 2</span>
+          <span className={styles.title}>SELECT PLAN</span>
+        </div>
+      </div>
+      <div className={styles.texts}>
+        <span className={styles.texts_number}>3</span>
+        <div>
+          <span className={styles.step}>STEP 3</span>
+          <span className={styles.title}>ADD-ONS</span>
+        </div>
+      </div>
+      <div className={styles.texts}>
+        <span className={styles.texts_number}>4</span>
+        <div>
+          <span className={styles.step}>STEP 4</span>
+          <span className={styles.title}>SUMMARY</span>
         </div>
       </div>
     </div>
