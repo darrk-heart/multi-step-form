9ac6f55e2c6ef4e63385c65a19cb93ea6a90a7ac
darrk-heart <prospaul999@gmail.com>
2024-03-18 09:29:59 +0000

    needed to format page

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import styles from "./Heading.module.css";
+import wallpaper from "../../assets/bg-sidebar-desktop.svg";
 
 function Heading() {
-  return <div>Heading</div>;
+  return (
+    <div className={styles.background}>
+      <img src={wallpaper} alt="background side bar" />
+      <div className={styles.text}>textss</div>
+    </div>
+  );
 }
 
 export default Heading;
