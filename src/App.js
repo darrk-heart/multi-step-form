928a0d37dcae482511d038e4c0832e41ab9f0a8f
darrk-heart <prospaul999@gmail.com>
2024-03-18 09:05:18 +0000

    building pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import Form1 from "./components/Form1/Form1";
+import Heading from "./components/Heading/Heading";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Form1>
+        <Heading />
+      </Form1>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Form1/Form1.jsx
+++ src/components/Form1/Form1.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import styles from "./Form1.module.css";
 
-function Form1() {
-  return <div>Form1</div>;
+function Form1({ children }) {
+  return (
+    <>
+      <div className={styles.background}>
+        {children}
+        <div>Hello WOrld</div>
+      </div>
+    </>
+  );
 }
 
 export default Form1;
