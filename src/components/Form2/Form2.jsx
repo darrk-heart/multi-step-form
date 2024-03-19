5a69988d56a7e2b2176449a46f6fc0abe472e833
darrk-heart <prospaul999@gmail.com>
2024-03-19 09:11:57 +0000

    page 2 under construction

--- src/components/Form2/Form2.jsx
+++ src/components/Form2/Form2.jsx
@@ -6,7 +6,16 @@ function Form2({ children }) {
     <>
       <div className={styles.background}>
         {children}
-        <div>Hello WOrld</div>
+        <div className={styles.form2bg}>
+          <div className={styles.maintext}>
+            <h1>Select your plan</h1>
+            <p>You have the option of monthly or yearly billing.</p>
+          </div>
+          <div className={styles.inputs}></div>
+          <div className={styles.nextstep}>
+            <span>Next Step</span>
+          </div>
+        </div>
       </div>
     </>
   );
