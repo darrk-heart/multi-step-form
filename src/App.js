3277e49b43455d89ef10a4a5162169b872921694
darrk-heart <prospaul999@gmail.com>
2024-03-19 09:05:10 +0000

    working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import Form1 from "./components/Form1/Form1";
+import Form2 from "./components/Form2/Form2";
 import Heading from "./components/Heading/Heading";
 
 function App() {
   return (
     <div className="App">
-      <Form1>
-        <Heading bg="#bde3ff" color="blue" border="#bde3ff" />
-      </Form1>
+      <Form2>
+        <Heading bg2="#bde3ff" color2="blue" border2="#bde3ff" />
+      </Form2>
     </div>
   );
 }
--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -3,14 +3,27 @@ import styles from "./Heading.module.css";
 import wallpaper from "../../assets/bg-sidebar-desktop.svg";
 
 function Heading(props) {
-  const { color, bg, border } = props;
+  const {
+    color1,
+    bg1,
+    border1,
+    color2,
+    bg2,
+    border2,
+    color3,
+    bg3,
+    border3,
+    color4,
+    bg4,
+    border4,
+  } = props;
 
   return (
     <div className={styles.background}>
       <img src={wallpaper} alt="background side bar" />
       <div className={styles.texts}>
         <span
-          style={{ borderColor: border, backgroundColor: bg, color: color }}
+          style={{ borderColor: border1, backgroundColor: bg1, color: color1 }}
           className={styles.texts_number}
         >
           1
@@ -21,21 +34,36 @@ function Heading(props) {
         </div>
       </div>
       <div className={styles.texts}>
-        <span className={styles.texts_number}>2</span>
+        <span
+          style={{ borderColor: border2, backgroundColor: bg2, color: color2 }}
+          className={styles.texts_number}
+        >
+          2
+        </span>
         <div>
           <span className={styles.step}>STEP 2</span>
           <span className={styles.title}>SELECT PLAN</span>
         </div>
       </div>
       <div className={styles.texts}>
-        <span className={styles.texts_number}>3</span>
+        <span
+          style={{ borderColor: border3, backgroundColor: bg3, color: color3 }}
+          className={styles.texts_number}
+        >
+          3
+        </span>
         <div>
           <span className={styles.step}>STEP 3</span>
           <span className={styles.title}>ADD-ONS</span>
         </div>
       </div>
       <div className={styles.texts}>
-        <span className={styles.texts_number}>4</span>
+        <span
+          style={{ borderColor: border4, backgroundColor: bg4, color: color4 }}
+          className={styles.texts_number}
+        >
+          1
+        </span>
         <div>
           <span className={styles.step}>STEP 4</span>
           <span className={styles.title}>SUMMARY</span>
