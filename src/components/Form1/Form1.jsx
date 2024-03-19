29749dfa2af82b8be5490cf042dc8dcc256b02dd
darrk-heart <prospaul999@gmail.com>
2024-03-19 08:30:55 +0000

    form1 done

--- src/components/Form1/Form1.jsx
+++ src/components/Form1/Form1.jsx
@@ -6,22 +6,22 @@ function Form1({ children }) {
     <>
       <div className={styles.background}>
         {children}
-        <div>
-          <div>
-            <h1>Personal Info</h1>
+        <div className={styles.form1bg}>
+          <div className={styles.maintext}>
+            <h1>Personal info</h1>
             <p>Please provide your name, email address, and phone number.</p>
           </div>
-          <div>
-            <div>
-              <span>Name</span>
-              <input placeholder="e.g. Prosper Paul" />
-              <span>Email Address</span>
-              <input placeholder="e.g. prosperpaul@gmail.com" />
-              <span>Phone Number</span>
-              <input placeholder="e.g. +1 234 567 890" />
-            </div>
+          <div className={styles.inputs}>
+            <span>Name</span>
+            <input placeholder="e.g. Prosper Paul" />
+            <span>Email Address</span>
+            <input placeholder="e.g. prosperpaul@gmail.com" />
+            <span>Phone Number</span>
+            <input placeholder="e.g. +1 234 567 890" />
           </div>
-          <div>Next Step</div>
+          <div className={styles.nextstep}>
+            <span>Next Step</span>
+          </div>
         </div>
       </div>
     </>
