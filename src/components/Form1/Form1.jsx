c552eb07773aa79302a101b9c003052e86f0d1dc
darrk-heart <prospaul999@gmail.com>
2024-03-19 07:08:41 +0000

    cooking

--- src/components/Form1/Form1.jsx
+++ src/components/Form1/Form1.jsx
@@ -6,7 +6,23 @@ function Form1({ children }) {
     <>
       <div className={styles.background}>
         {children}
-        <div>Hello WOrld</div>
+        <div>
+          <div>
+            <h1>Personal Info</h1>
+            <p>Please provide your name, email address, and phone number.</p>
+          </div>
+          <div>
+            <div>
+              <span>Name</span>
+              <input placeholder="e.g. Prosper Paul" />
+              <span>Email Address</span>
+              <input placeholder="e.g. prosperpaul@gmail.com" />
+              <span>Phone Number</span>
+              <input placeholder="e.g. +1 234 567 890" />
+            </div>
+          </div>
+          <div>Next Step</div>
+        </div>
       </div>
     </>
   );
