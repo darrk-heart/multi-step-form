b28c3fcb6b7d034bcc1a46da8767d992e1471c5c
darrk-heart <prospaul999@gmail.com>
2024-03-20 03:38:03 +0000

    page3 under construction

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
 import Form1 from "./components/Form1/Form1";
 import Form2 from "./components/Form2/Form2";
+import Form3 from "./components/Form3/Form3";
 import Heading from "./components/Heading/Heading";
 
 function App() {
   return (
     <div className="App">
-      <Form2>
-        <Heading bg2="#bde3ff" color2="blue" border2="#bde3ff" />
-      </Form2>
+      <Form3>
+        <Heading bg3="#bde3ff" color3="blue" border3="#bde3ff" />
+      </Form3>
     </div>
   );
 }
--- src/components/Form3/Form3.jsx
+++ src/components/Form3/Form3.jsx
@@ -1,7 +1,63 @@
 import React from "react";
+import styles from "./Form3.module.css";
+import check from "../../assets/icon-checkmark.svg";
 
-function Form3() {
-  return <div>Form3</div>;
+function Form3({ children }) {
+  return (
+    <>
+      <div className={styles.background}>
+        {children}
+        <div className={styles.form3bg}>
+          <div className={styles.maintext}>
+            <h1>Pick add-ons</h1>
+            <p>Add-ons help enhance your gaming experience.</p>
+          </div>
+          <div className={styles.main}>
+            <div className={styles.selects}>
+              <img
+                src={check}
+                alt="checkmar"
+                style={{ backgroundColor: "blue" }}
+              />
+              <div className={styles.selectText}>
+                <h4>Online service</h4>
+                <p>Access to multiplayer games</p>
+              </div>
+              <p>+$1/mo</p>
+            </div>
+            <div className={styles.selects}>
+              <img
+                src={check}
+                alt="checkmar"
+                style={{ backgroundColor: "blue" }}
+              />
+              <div className={styles.selectText}>
+                <h4>Larger storage</h4>
+                <p>Extra 1TB of cloud save</p>
+              </div>
+              <p>+$1/mo</p>
+            </div>
+            <div className={styles.selects}>
+              <img
+                src={check}
+                alt="checkmar"
+                style={{ backgroundColor: "blue" }}
+              />
+              <div className={styles.selectText}>
+                <h4>Cuztomizable profile</h4>
+                <p>Custom theme on your profile</p>
+              </div>
+              <p>+$1/mo</p>
+            </div>
+          </div>
+          <div className={styles.beneath}>
+            <span>Go Back</span>
+            <h6 className={styles.nextstep}>Next Step</h6>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Form3;
